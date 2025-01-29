6b02208812fd0a155a100b8d9b2338f971e78733
dlzvy <119075598+dlzvy@users.noreply.github.com>
2025-01-29 17:32:30 +0700

    Add files via upload

---
+++ logo.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo -e "\e[1;33m
+  SSSSS   CCCCC   AAAAA  V   V  EEEEE  N   N  GGGG  EEEEE  RRRR     AAAAA  III  RRRR   DDDD   RRRR   OOO   PPPP  
+ S        C       A   A  V   V  E      NN  N  G      E      R   R   A   A   I   R   R  D   D  R   R O   O  P   P 
+  SSS     C       AAAAA  V   V  EEEE   N N N  G  GG  EEEE   RRRR    AAAAA   I   RRRR   D   D  RRRR  O   O  PPPP  
+     S    C       A   A  V   V  E      N  NN  G   G  E      R  R    A   A   I   R  R   D   D  R  R  O   O  P      
+  SSSS     CCCCC   A   A   VVV   EEEEE  N   N  GGGG  EEEEE  R   R   A   A  III  R   R  DDDD   R   R  OOO   P      
+                  
+\e[1;34m
+               Join the scavenger airdrop Now!\e[1;32m
+        ──────────────────────────────────────
+        Telegram Group: \e[1;4;33mhttps://t.me/scavengerairdrop\e[0m
+        ──────────────────────────────────────"
+
+
+
+
+
